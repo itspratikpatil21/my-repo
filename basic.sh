36c55715dfe7022353fbbacb13df9de001ee666e
PratikPatil2121 <148964926+PratikPatil2121@users.noreply.github.com>
2025-01-09 10:58:29 +0530

    bash file added

---
+++ basic.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+#echo "helllloo!!!!!!!!!!!!!! enter ur name"
+#read name
+
+#echo "hello $name"
+echo "enter number"
+read num
+
+if [ $num -gt 10 ]; then
+  echo "the number is greater than 10"
+else 
+  echo "the number is less than or equal to 10"
+fi 
